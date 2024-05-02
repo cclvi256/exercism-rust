c4ab8126fb6a9e97a8eab5c51edcddd932504ea2
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-05-03 00:47:43 +0800

    Add _ to separate per 3 digits

--- src/gigasecond.rs
+++ src/gigasecond.rs
@@ -2,5 +2,5 @@ use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start + time::Duration::seconds(1000000000)
+    start + time::Duration::seconds(1_000_000_000)
 }
