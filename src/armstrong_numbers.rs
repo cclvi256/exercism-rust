d90c75abbe8f719accdb9429ebcdb3d90b472be2
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-05-03 01:07:31 +0800

    Add practice Armstrong number

---
+++ src/armstrong_numbers.rs
@@ -0,0 +1,21 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    let separated_digits = digits(num);
+    let digit_count = separated_digits.len();
+    let mut sum: u64 = 0;
+    for digit in separated_digits {
+        sum += digit.pow(digit_count as u32) as u64;
+    }
+    sum == num as u64
+}
+
+fn digits (n: u32) -> Vec<u32> {
+    let mut digits = Vec::new();
+    let mut n = n;
+    while n > 0 {
+        digits.push(n % 10);
+        n /= 10;
+    }
+    digits
+}
+
+// Sometimes it's necessary to use 64-bit integers.
