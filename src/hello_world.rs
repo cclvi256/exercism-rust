12c3481aab8ad178bd76553a025a124f7be8d339
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-05-04 23:47:13 +0800

    Try to fix broken structure

--- src/hello_world.rs
+++
@@ -1,9 +0,0 @@
-// &'static is a "lifetime specifier", something you'll learn more about later
-pub fn hello() -> &'static str {
-    "Hello, World!"
-}
-
-#[test]
-fn hello_test() {
-    assert_eq!(hello(), "Hello, World!");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-mod armstrong_numbers;
-mod beer_song;
-mod difference_of_squares;
-mod gigasecond;
-mod grains;
-mod hello_world;
-mod leap;
-mod matching_brackets;
-mod nth_prime;
-mod prime_factors;
-mod raindrops;
-mod reverse_string;
-mod clock;
-
-fn main() {}
+fn main() {
+    println!("Hello, world!");
+}
